f002a75459cf1e6a7d03dcf2404a69aac3b04522
Julio Alejo <julio.alejo.herranz@alumnos.upm.es>
2018-11-29 02:07:14 +0100

    Añadido for para sacar los parametros de una linea

--- configurar_cluster.sh
+++ configurar_cluster.sh
@@ -8,11 +8,32 @@ then
 	exit 1
 fi 
 
-
 F_CONF=$1
+F_C_FORMAT=`grep -E -v '^(#|$)' $F_CONF`
 
-F_C_FORMAT=`grep -E -v '^(#|$)' $1`
+Num_LIN=1 
+IFS=$'\n' 
 
- echo $F_C_FORMAT
+# 1  
+#for servicio in $F_C_FORMAT; do
+#	#Hacemos que el espacio separe las variables
+#	IFS=$' '
+#	echo $Num_LIN $servicio   # Sacamos Nº linea, máquina, servicio, fichero de conf serv 
+#	let Num_LIN+=1
+#done
+
+# 2
+for service in $F_C_FORMAT 
+do
+	IFS=$' '
+    IP=`echo $service | cut -d " " -f1` 
+    SERVICE=`echo $service | cut -d " " -f2` 
+ 	FILE_CONF_SERV=`echo $service | cut -d " " -f3`
+
+    echo $Num_LIN $IP $SERVICE $FILE_CONF_SERV 
+    # funcion $Num_LIN $IP $SERVICE $FILE_CONF_SERV
+    let Num_LIN+=1
+done
+ 
 
 exit 0
