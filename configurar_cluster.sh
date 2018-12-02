54ad8f837e721245e3303cc4f9ededec171a8e10
Julio Alejo <julio.alejo.herranz@alumnos.upm.es>
2018-12-02 15:15:46 +0100

    Case tratamineto del servicio

--- configurar_cluster.sh
+++ configurar_cluster.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-# Comprobación número de argumentos
+# Check number of arguments
 
 if [ $# -ne 1 ]
 then 
-	echo "Uso: El número de argumnetos no es válido"
+	echo "ERROR: El número de argumentos no es válido"
 	exit 1
 fi 
+
+# Check files  
+
+file_service="mount.sh raid.sh lvm.sh nis_server.sh nis_client.sh nfs_server.sh nfs_client.sh backup_client.sh backup_server.sh"
+
+for file in $file_service; do
+	if [ ! -f "./Service/$file" ]
+	then
+		echo "ERROR: Fichero $file no disponible."
+		exit 1
+	fi
+done
+
 
 F_CONF=$1
 F_C_FORMAT=`grep -E -v '^(#|$)' $F_CONF`
@@ -14,25 +27,51 @@ F_C_FORMAT=`grep -E -v '^(#|$)' $F_CONF`
 Num_LIN=1 
 IFS=$'\n' 
 
-# 1  
-#for servicio in $F_C_FORMAT; do
-#	#Hacemos que el espacio separe las variables
-#	IFS=$' '
-#	echo $Num_LIN $servicio   # Sacamos Nº linea, máquina, servicio, fichero de conf serv 
-#	let Num_LIN+=1
-#done
 
-# 2
 for service in $F_C_FORMAT 
 do
 	IFS=$' '
-    IP=`echo $service | cut -d " " -f1` 
-    SERVICE=`echo $service | cut -d " " -f2` 
- 	FILE_CONF_SERV=`echo $service | cut -d " " -f3`
+	IP=`echo $service | cut -d " " -f1` 
+	SERVICE=`echo $service | cut -d " " -f2` 
+	FILE_CONF_SERV=`echo $service | cut -d " " -f3`
 
-    echo $Num_LIN $IP $SERVICE $FILE_CONF_SERV 
-    # funcion $Num_LIN $IP $SERVICE $FILE_CONF_SERV
-    let Num_LIN+=1
+	echo "Parametros de la linea: $Num_LIN $IP $SERVICE $FILE_CONF_SERV" 
+   
+	let Num_LIN+=1
+
+	case $SERVICE in
+		mount )
+			SCRIPT=mount.sh
+			;;
+		raid )
+			SCRIPT=raid.sh
+			;;
+		lvm )
+			SCRIPT=lvm.sh
+			;;
+		nis_server )
+			SCRIPT=nis_server.sh
+			;;
+		nis_client )
+			SCRIPT=nis_client.sh
+			;;
+		nfs_server )
+			SCRIPT=nfs_server.sh
+			;;
+		nfs_client )
+			SCRIPT=nfs_client.sh
+			;;
+		backup_server )
+			SCRIPT=backup_server.sh
+			;;
+		backup_client )
+			SCRIPT=backup_client.sh
+			;;
+		*)
+			echo "ERROR: Tratamiento del servicio $SERVICE"
+			exit 1
+			;;
+	esac
 done
  
 
