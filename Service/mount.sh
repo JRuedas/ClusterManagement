08c1533402f378840eb361f80fba146c70c280f8
Julio Alejo <julio.alejo.herranz@alumnos.upm.es>
2018-12-07 13:44:35 +0100

    Mount.sh v1.2.2

--- Service/mount.sh
+++ Service/mount.sh
@@ -5,7 +5,7 @@
 
 File_Conf_Serv=$1
 Name_Dev=`head -n 1 $File_Conf_Serv`
-Mount_Point=`tail -n 2 $File_Conf_Serv`
+Mount_Point=`tail -n 1 $File_Conf_Serv`
 
 echo "Nombre del dispositivo: $Name_Dev"
 echo "Punto de montaje: $Mount_Point"
