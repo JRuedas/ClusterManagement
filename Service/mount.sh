bc249781e6c8445b56aba3a3004ec9348870b984
Julio Alejo <julio.alejo.herranz@alumnos.upm.es>
2018-12-07 13:21:46 +0100

    Mount.sh v1.2

--- Service/mount.sh
+++ Service/mount.sh
@@ -12,17 +12,45 @@ echo "Punto de montaje: $Mount_Point"
 
 # Check if device exist
 
-isMounted=`df | grep -o $Name_Dev` 
+isMounted=`df | grep -o "$Name_Dev"` 
 
-if [ -z "$Name_Dev"] 
+if [ -z "$isMounted" ] 
 then
 	echo "Service_MOUNT: Error de montado, el dispositivo $Name_Dev no existe o no se puede montar"
 fi
 
 echo "Valor de dispositivo $isMounted"
 
+# Check mount point 
+
+if [ -z "$Mount_Point" ] 
+then
+	echo "Service_MOUNT: Error de montado, falta de punto de montaje"
+fi
+
 # Creat a directory with intermediate
 
 mkdir -p $Mount_Point > /dev/null
+
+# Check configuration on fstab and mount
+
+grep -q "$Name_Dev" /etc/fstab 
+
+if [ $? -ne 0 ]
+then 
+ 	echo "$Name_Dev[\t]$Mount_Point[\t]auto[\t]auto[\t]0[\t]0" >> /etc/fstab 
+ elif [ $? -ne 0 ]
+ 	echo "Service_MOUNT: Error no se ha podido configurar /etc/fstab"
+else 
+	echo "Service_MOUNT: El dispositivo $Name_Dev ya se encuentra en /etc/fstab"
+fi
+
+# mount all file systems mentioned in fstab
+
+mount -a > /dev/null
+
+echo "Service_MOUNT: Servicio Montaje completado correctamente"
+
+
 
 
