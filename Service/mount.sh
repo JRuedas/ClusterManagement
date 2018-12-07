c509160d24ecb5e5e78ac913d26cc6e13ee0da21
Julio Alejo <julio.alejo.herranz@alumnos.upm.es>
2018-12-07 02:33:03 +0100

    Añadido la comprobación de existencia de dispositivo y creación de directorio con intermediarios

--- Service/mount.sh
+++ Service/mount.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 # File of the service mount 
 
-linea=0
-for comand in $(cat $1); do
-	if [ $linea = 0 ]
-	then
-		#Nombre Dispositivo
-		NOMBRE_DEL_DISPOSITIVO=$comand
-	elif [ $linea = 1 ]
-	then
-		#Punto de Montaje
-		PUNTO_DE_MONTAJE=$comand
-	fi
-	let linea+=1
-done
-
-
-echo "Nombre del dispositivo: $NOMBRE_DEL_DISPOSITIVO"
-echo "Punto de montaje: $PUNTO_DE_MONTAJE"
+#Get parameters from the service configuration file
 
 File_Conf_Serv=$1
 Name_Dev=`head -n 1 $File_Conf_Serv`
@@ -26,11 +10,19 @@ Mount_Point=`tail -n 2 $File_Conf_Serv`
 echo "Nombre del dispositivo: $Name_Dev"
 echo "Punto de montaje: $Mount_Point"
 
-if [ "$PUNTO_DE_MONTAJE" = "$Mount_Point" ]
-then 
-	echo "Son iguales"
-else
-	echo "No sonn iguales"
+# Check if device exist
+
+isMounted=`df | grep -o $Name_Dev` 
+
+if [ -z "$Name_Dev"] 
+then
+	echo "Service_MOUNT: Error de montado, el dispositivo $Name_Dev no existe o no se puede montar"
 fi
+
+echo "Valor de dispositivo $isMounted"
+
+# Creat a directory with intermediate
+
+mkdir -p $Mount_Point > /dev/null
 
 
