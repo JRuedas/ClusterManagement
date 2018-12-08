eafa874a2fea355eab7c5e68fa79f116f0f76750
letylo <hbl.leticia@gmail.com>
2018-12-08 10:08:48 +0100

    Finish NIS server

--- Service/nis_server.sh
+++ Service/nis_server.sh
@@ -6,17 +6,17 @@ NIS_DOMAIN_NAME=$(cat $FILE_CONF)
 # Check number of arguments
 if [ $# -ne 1 ]
 then
-	echo "ERROR: El número de argumentos no es válido"
+	echo "\nERROR: El número de argumentos no es válido"
 	exit 1
 else
 	#Check if the param is a file
 	if [ -f $FILE_CONF ]
 	then 
-		echo "El contenido del fichero de configuración es: "
+		echo "\nEl contenido del fichero de configuración es: "
 		echo $NIS_DOMAIN_NAME
 
 	else
-		echo "ERROR: $FILE_CONF no existe o no es un fichero"
+		echo "\nERROR: $FILE_CONF no existe o no es un fichero"
 		exit 1
 	fi 
 fi 
@@ -25,21 +25,30 @@ fi
 #install the tools
 #apt-get update
 #apt-get install nis
-#echo "Heramientas para crear el RAID instaladas"
+#echo "\nHeramientas para crear el servidor NIS instaladas"
 
 #Modified the config files
 
 sed '1d' /etc/defaultdomain > test-file.txt
 echo $NIS_DOMAIN_NAME > /etc/defaultdomain
 rm test-file.txt
-echo "El fichero /etc/defaultdomain ha sido modificado, su contenido ahora es: " 
+echo "\nEl fichero /etc/defaultdomain ha sido modificado, su contenido ahora es: " 
 cat /etc/defaultdomain
 
 sed 's/NISSERVER=false/NISSERVER=true/g' /etc/default/nis > test-file.txt
-sed 's/NISCLIENT=true/NISCLIENT=false/g' test-file.txt > test-file-final.txt
-rm /etc/default/nis
+sed 's/NISCLIENT=true/NISCLIENT=false/g' test-file.txt > /etc/default/nis
 rm test-file.txt
-mv test-file-final.txt /etc/default/nis
-
-echo "El fichero /etc/default/nis ha sido modificado, su contenido ahora es: " 
+echo "\nEl fichero /etc/default/nis ha sido modificado, su contenido ahora es: " 
 cat /etc/default/nis
+
+# modificar y poner dir broadcast y dir de red /etc/ypserv.securenets
+
+sed 's/127.0.1.1$/127.0.1.1 '$NIS_DOMAIN_NAME' /g' /etc/hosts > test-file.txt
+cat test-file.txt | /etc/hosts
+rm test-file.txt
+echo "\nEl fichero /etc/hosts ha sido modificado, su contenido ahora es: " 
+cat /etc/hosts
+
+#Comprobar grupos y UID de usuarios para tener acceso al server /var/yp/Makefile
+
+
