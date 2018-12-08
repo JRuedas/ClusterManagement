e030bdea853c28aa55504c551cb55b1e44ac07c5
root <root@ASI2014>
2018-12-08 11:40:07 +0100

    Fix etc/hosts

--- Service/nis_server.sh
+++ Service/nis_server.sh
@@ -29,38 +29,36 @@ fi
 
 #Modified the config files
 
-sed '1d' /etc/defaultdomain > test-file.txt
 echo $NIS_DOMAIN_NAME > /etc/defaultdomain
-rm test-file.txt
 echo "\nEl fichero /etc/defaultdomain ha sido modificado, su contenido ahora es: " 
 cat /etc/defaultdomain
 
-sed 's/NISSERVER=$/NISSERVER=true/g' /etc/default/nis > test-file.txt
-sed 's/NISCLIENT=$/NISCLIENT=false/g' test-file.txt > /etc/default/nis
+sed 's/NISSERVER=.*/NISSERVER=true/g' /etc/default/nis > test-file.txt
+sed 's/NISCLIENT=.*/NISCLIENT=false/g' test-file.txt > /etc/default/nis
 rm test-file.txt
 echo "\nEl fichero /etc/default/nis ha sido modificado, su contenido ahora es: " 
 cat /etc/default/nis
 
 # modificar y poner dir broadcast y dir de red /etc/ypserv.securenets
 
-sed 's/127.0.1.1$/127.0.1.1 '$NIS_DOMAIN_NAME' /g' /etc/hosts > test-file.txt
+sed 's/127.0.1.1 .*/127.0.1.1 '$NIS_DOMAIN_NAME'/g' /etc/hosts > test-file.txt
 cat test-file.txt > /etc/hosts
 rm test-file.txt
 echo "\nEl fichero /etc/hosts ha sido modificado, su contenido ahora es: " 
 cat /etc/hosts
 
-#sed 's/^MERGE_PASSWD=.*/MERGE_PASSWD=true/g' /var/yp/Makefile > test-file.txt
-#sed 's/^MERGE_GROUP=.*/MERGE_GROUP=true/g' test-file.txt > test-file2.txt
-#echo "\nEl fichero /var/yp/Makefile ha sido modificado, su contenido ahora es: " 
-#sed -n '/MERGE_PASSWD=/p' test-file2.txt
-#sed -n '/MERGE_GROUP=/p' test-file2.txt
+sed 's/^MERGE_PASSWD=.*/MERGE_PASSWD=true/g' /var/yp/Makefile > test-file.txt
+sed 's/^MERGE_GROUP=.*/MERGE_GROUP=true/g' test-file.txt > test-file2.txt
+echo "\nEl fichero /var/yp/Makefile ha sido modificado, su contenido ahora es: " 
+sed -n '/MERGE_PASSWD=/p' test-file2.txt
+sed -n '/MERGE_GROUP=/p' test-file2.txt
 
 
-#/usr/lib/yp/ypinit -m 
-#echo "\nInformación de /etc volcada a repositorio" 
+/usr/lib/yp/ypinit -m 
+echo "\nInformación de /etc volcada a repositorio" 
 
-#make -C /var/yp
-#echo "\nActualizado repositorio"
+make -C /var/yp
+echo "\nActualizado repositorio"
 
 
 
