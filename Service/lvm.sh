b1cc7d07d925dc6bfd042270331d529b45d3a14b
Jonathan <jonathanruedasmora@gmail.com>
2018-12-11 00:27:55 +0100

    Adds LVM script and its config file. TODO: Check disk size before create volume. Check exist physical devices. Check correct format

--- Service/lvm.sh
+++ Service/lvm.sh
@@ -1,2 +1,55 @@
-# File of the service lvm 
+#!/bin/bash
 
+prevIFS=$IF
+IFS=$'\n'
+
+echo "Actualizando paquetes e instalando servicio LVM version 2"
+#apt-get update && apt-get install lvm2 -qq --force-yes > /dev/null
+
+echo "Leyendo fichero de configuración del servicio"
+
+linecount=0
+volcount=0
+for line in $(cat $1); do
+
+	# Skip comments if any
+	if [[ $line == *"#"* ]]; then
+		continue
+	fi
+
+	if [ $linecount = 0 ]; then
+		NAME=$line
+		echo "Nombre: " $NAME
+	elif [ $linecount = 1 ]; then
+		DEVICES=$line
+		echo "Devices: " $DEVICES
+	elif [ $linecount > 1 ]; then
+		IFS=' '
+	       	read -r -a array <<< "$line"
+		VOL_NAME[$volcount]=${array[0]}
+		VOL_SIZE[$volcount]=${array[1]}
+		echo "Vol name" $volcount ${VOL_NAME[$volcount]}
+	        echo "Vol size"	$volcount ${VOL_SIZE[$volcount]} 
+		IFS=$'\n'
+		let volcount+=1
+	fi
+	let linecount+=1
+done
+
+echo "Inicializando los volumenes fisicos"
+pvcreate $DEVICES > /dev/null
+
+echo "Creando grupo de volumenes"
+vgcreate $NAME $DEVICES > /dev/null
+
+echo "Creando volumenes logicos dentro del grupo"
+
+counter=0
+until [ $counter -ge $volcount ]; do
+	lvcreate --name $VOL_NAME[$counter] --size $VOL_SIZE[$counter] $NAME > /dev/null
+	let counter+=1
+     done
+     
+IFS=$prevIFS
+
+echo "Servicio configurado"
