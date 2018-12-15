70af7bf0edaa94320c9a70875628b62ad52d5528
letylo <root@ASI2014>
2018-12-15 14:55:25 +0100

    Fix /etc/hosts

--- Service/nis_server.sh
+++ Service/nis_server.sh
@@ -23,29 +23,34 @@ fi
 
 
 #install the tools
-#apt-get update
-#apt-get install nis
-#echo "\nHeramientas para crear el servidor NIS instaladas"
-
-#Modified the config files
+apt-get update
+apt-get install nis
+if [ $? -eq 0 ]
+    then echo "\nHeramientas para crear el servidor NIS instaladas"
+else
+    echo "\nError al instalar NIS"
+fi
 
 echo $NIS_DOMAIN_NAME > /etc/defaultdomain
 echo "\nEl fichero /etc/defaultdomain ha sido modificado, su contenido ahora es: " 
 cat /etc/defaultdomain
 
-sed 's/NISSERVER=.*/NISSERVER=true/g' /etc/default/nis > test-file.txt
+sed 's/NISSERVER=.*/NISSERVER=master/g' /etc/default/nis > test-file.txt
 sed 's/NISCLIENT=.*/NISCLIENT=false/g' test-file.txt > /etc/default/nis
 rm test-file.txt
 echo "\nEl fichero /etc/default/nis ha sido modificado, su contenido ahora es: " 
 cat /etc/default/nis
 
 # modificar y poner dir broadcast y dir de red /etc/ypserv.securenets
+cat /etc/ypserv.securenets
+echo "\n"
 
-sed 's/127.0.1.1 .*/127.0.1.1 '$NIS_DOMAIN_NAME'/g' /etc/hosts > test-file.txt
+sed "/127.0.1.1/ s/127.0.1.1 .*/127.0.1.1 $NIS_DOMAIN_NAME/g" /etc/hosts > test-file.txt
 cat test-file.txt > /etc/hosts
 rm test-file.txt
 echo "\nEl fichero /etc/hosts ha sido modificado, su contenido ahora es: " 
 cat /etc/hosts
+
 
 sed 's/^MERGE_PASSWD=.*/MERGE_PASSWD=true/g' /var/yp/Makefile > test-file.txt
 sed 's/^MERGE_GROUP=.*/MERGE_GROUP=true/g' test-file.txt > test-file2.txt
@@ -54,12 +59,28 @@ sed -n '/MERGE_PASSWD=/p' test-file2.txt
 sed -n '/MERGE_GROUP=/p' test-file2.txt
 
 
+# cambiar el nombre del servidor nis
+sed "s/.*/$NIS_DOMAIN_NAME/g" /var/yp/ypservers > test-file.txt
+cat test-file.txt > /var/yp/ypservers
+rm test-file.txt
+echo "\nEl fichero /var/yp/ypservers ha sido modificado, su contenido ahora es: "
+cat /var/yp/ypservers
+
 /usr/lib/yp/ypinit -m 
-echo "\nInformación de /etc volcada a repositorio" 
-
+if [ $? -eq 0 ]
+    then echo "\nInformación de configuración volcada a repositorio"
+else
+    echo "\nError al volcar la información al repositorio"
+fi
+ 
 make -C /var/yp
-echo "\nActualizado repositorio"
+if [ $? -eq 0 ]
+    then echo "\nRepositorio actualizado"
+else
+    echo "\nError al actualizar repositorio"
+fi
 
+#duda como sustituir hostname en actualizacion de fichero
 
 
 
