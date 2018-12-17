0259400d3d6677ae6d043db1ab9ea3cff6673fe3
letylo <hbl.leticia@gmail.com>
2018-12-17 17:39:07 +0100

    It only remains to make tests

--- Service/nis_client.sh
+++ Service/nis_client.sh
@@ -4,37 +4,38 @@ FILE_CONF=$*
 NIS_DOMAIN=$(head -n 1 $FILE_CONF)
 NIS_SERVER=$(tail -n 1 $FILE_CONF)
 
-# Check number of arguments
+#check number of arguments
 if [ $# -ne 1 ]
 then
 	echo "\nERROR: El número de argumentos no es válido"
 	exit 1
 else
-	#Check if the param is a file
+	#check if the param is a file
 	if [ -f $FILE_CONF ]
 	then 
 		echo "\nEl contenido del fichero de configuración es: "
 		cat $FILE_CONF
 
 	else
-		echo "\nERROR: $FILE_CONF no existe o no es un fichero"
+		echo "\nERROR: $FILE_CONF no es un fichero"
 		exit 1
 	fi 
 fi 
 
+
 #install the tools
-#apt-get update
+echo "\nInstalando heramientas para crear el cliente NIS..."
+apt-get update
 apt-get -y install nis
 if [ $? -eq 0 ]
-    then echo "\nHeramientas para crear el servidor NIS instaladas"
+    then echo "\nHeramientas para crear el cliente NIS instaladas"
 else
     echo "\nError al instalar NIS"
 fi
 
 
-#modified config files
-
-#modified /etc/default/nis
+#modified /etc/default/nis to set the client role
+echo "\nModificando fichero /etc/default/nis para establecer el rol de cliente... " 
 sed 's/NISSERVER=.*/NISSERVER=false/g' /etc/default/nis > test-file.txt
 sed 's/NISCLIENT=.*/NISCLIENT=true/g' test-file.txt > /etc/default/nis
 rm test-file.txt
@@ -42,14 +43,16 @@ echo "\nEl fichero /etc/default/nis ha sido modificado, su contenido ahora es: "
 cat /etc/default/nis
 
 
-#modified /etc/yp.conf
+#modified /etc/yp.conf to localize the servers
+echo "\nModificando fichero /etc/yp.conf para localización de servidores... " 
 sed "/ypserver ip-servidor-nis/ s/ypserver ip-servidor-nis/$NIS_DOMAIN $NIS_SERVER/g" /etc/yp.conf > test-file.txt
 cat test-file.txt > /etc/yp.conf
 echo "\nEl fichero /etc/yp.conf ha sido modificado, su contenido ahora es: " 
 cat /etc/yp.conf
 
 
-#modified /etc/nsswitch.conf
+#modified /etc/nsswitch.conf to set what info is to use NIS
+echo "\nModificando fichero /etc/nsswitch.conf para establecer que información se usa en NIS... " 
 sed '/^passwd:         compat$/ s/passwd:         compat/passwd:         compat nis/g' /etc/nsswitch.conf > test-file.txt
 sed '/^group:          compat$/ s/group:          compat/group:          compat nis/g' test-file.txt > test-file2.txt
 sed '/^shadow:         compat$/ s/shadow:         compat/shadow:         compat nis/g' test-file2.txt > test-file3.txt
@@ -57,9 +60,10 @@ sed '/^hosts:          files dns$/ s/hosts:          files dns/hosts:          f
 rm test-file.txt
 rm test-file2.txt
 rm test-file3.txt
+echo "\nEl fichero /etc/nsswitch.conf ha sido modificado, su contenido ahora es: " 
 cat /etc/nsswitch.conf
 
-#duda:como arrancar nis client
+#start service
 
 
 
--- Service/nis_server.sh
+++ Service/nis_server.sh
@@ -3,48 +3,61 @@
 FILE_CONF=$*
 NIS_DOMAIN_NAME=$(cat $FILE_CONF)
 
-# Check number of arguments
+
+#check number of arguments
 if [ $# -ne 1 ]
 then
 	echo "\nERROR: El número de argumentos no es válido"
 	exit 1
 else
-	#Check if the param is a file
+	#check if the param is a file
 	if [ -f $FILE_CONF ]
 	then 
 		echo "\nEl contenido del fichero de configuración es: "
 		echo $NIS_DOMAIN_NAME
 
 	else
-		echo "\nERROR: $FILE_CONF no existe o no es un fichero"
+		echo "\nERROR: $FILE_CONF no es un fichero"
 		exit 1
 	fi 
 fi 
 
 
 #install the tools
+echo "\nInstalando heramientas para crear el servidor NIS..."
 apt-get update
-apt-get install nis
+apt-get install nis >> /dev/null
 if [ $? -eq 0 ]
     then echo "\nHeramientas para crear el servidor NIS instaladas"
 else
     echo "\nError al instalar NIS"
 fi
 
+
+#modified /etc/defaultdomain to set the domain server
+echo "\nModificando fichero /etc/defaultdomain para poner nombre de dominio NIS... " 
 echo $NIS_DOMAIN_NAME > /etc/defaultdomain
 echo "\nEl fichero /etc/defaultdomain ha sido modificado, su contenido ahora es: " 
 cat /etc/defaultdomain
 
+#modified /etc/default/nis to set the role server
+echo "\nModificando fichero /etc/default/nis para establecer servidor como master... " 
 sed 's/NISSERVER=.*/NISSERVER=master/g' /etc/default/nis > test-file.txt
 sed 's/NISCLIENT=.*/NISCLIENT=false/g' test-file.txt > /etc/default/nis
 rm test-file.txt
 echo "\nEl fichero /etc/default/nis ha sido modificado, su contenido ahora es: " 
 cat /etc/default/nis
 
-# modificar y poner dir broadcast y dir de red /etc/ypserv.securenets
+
+#modified /etc/ypserv.securenets to establish the interface for the machines have access
+#now give access to everybody, change to network interface
+echo "\nEl fichero /etc/ypserv.securenets ha sido modificado, su contenido ahora es: " 
 cat /etc/ypserv.securenets
 echo "\n"
 
+
+#modified /etc/hosts to set the domain server
+echo "\nModificando fichero /etc/hosts para poner servidor NIS como host... " 
 sed "/127.0.1.1/ s/127.0.1.1 .*/127.0.1.1 $NIS_DOMAIN_NAME/g" /etc/hosts > test-file.txt
 cat test-file.txt > /etc/hosts
 rm test-file.txt
@@ -52,35 +65,32 @@ echo "\nEl fichero /etc/hosts ha sido modificado, su contenido ahora es: "
 cat /etc/hosts
 
 
+#modified /var/yp/Makefile to set the passw and group in the repository
+echo "\nModificando fichero /var/yp/Makefile para que contraseñas esten en repositorio... " 
 sed 's/^MERGE_PASSWD=.*/MERGE_PASSWD=true/g' /var/yp/Makefile > test-file.txt
 sed 's/^MERGE_GROUP=.*/MERGE_GROUP=true/g' test-file.txt > test-file2.txt
 echo "\nEl fichero /var/yp/Makefile ha sido modificado, su contenido ahora es: " 
-sed -n '/MERGE_PASSWD=/p' test-file2.txt
-sed -n '/MERGE_GROUP=/p' test-file2.txt
+cat test-file2.txt > /var/yp/Makefile
+rm test-file.txt test-file2.txt
+sed -n '/MERGE_PASSWD=/p' /var/yp/Makefile
+sed -n '/MERGE_GROUP=/p' /var/yp/Makefile
 
 
-# cambiar el nombre del servidor nis
+#modified the name of the domain server
+echo "\nModificando fichero /var/yp/ypservers para volcar la información de configuración al repositorio... " 
 sed "s/.*/$NIS_DOMAIN_NAME/g" /var/yp/ypservers > test-file.txt
 cat test-file.txt > /var/yp/ypservers
 rm test-file.txt
 echo "\nEl fichero /var/yp/ypservers ha sido modificado, su contenido ahora es: "
 cat /var/yp/ypservers
 
-/usr/lib/yp/ypinit -m 
+
+#set the configuration info to the repository
+/usr/lib/yp/ypinit -m > /dev/null
 if [ $? -eq 0 ]
     then echo "\nInformación de configuración volcada a repositorio"
 else
     echo "\nError al volcar la información al repositorio"
 fi
- 
-make -C /var/yp
-if [ $? -eq 0 ]
-    then echo "\nRepositorio actualizado"
-else
-    echo "\nError al actualizar repositorio"
-fi
-
-#duda como sustituir hostname en actualizacion de fichero
-
 
 
