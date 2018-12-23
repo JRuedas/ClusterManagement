24999423a11d76c52e6a461f00361c23d439b9ae
Julio Alejo <julio.alejo.herranz@alumnos.upm.es>
2018-12-23 12:19:40 +0100

    Formato mount

--- Service/mount.sh
+++ Service/mount.sh
@@ -1,63 +1,77 @@
 #!/bin/bash
-# File of the service mount 
+ 
+echo "Servicio MOUNT: Iniciando servicio MOUNT"
 
-#Get parameters from the service configuration file
+# Comprobamos si el número de parámetros es correcto y el fichero de configuración existe
+
+if [ $# -ne 1 ]; then
+	echo "ERROR: Numero de parametros incorrecto"
+	exit 1
+fi
+
+if [ ! -f $1 ]; then
+	echo "ERROR: El fichero especificado no existe"
+	exit 1
+fi
+
+#Obtener los parámetros del fichero de perfil de configuración de servicio
 
 File_Conf_Serv=$1
 Name_Dev=`head -n 1 $File_Conf_Serv`
 Mount_Point=`sed -n 2p $File_Conf_Serv`  
-#Mount_Point=`tail -n 1 $File_Conf_Serv`
 
-echo "Nombre del dispositivo: $Name_Dev"
-echo "Punto de montaje: $Mount_Point"
 
-# Check if device exist
+echo "Servicio MOUNT: Nombre del dispositivo: $Name_Dev"
+echo "Servicio MOUNT: Punto de montaje: $Mount_Point"
+
+# Comprobar que el dispositivo existe
 
 isMounted=`ls -l /dev/sd* | grep -o $Name_Dev` 
  
 if [ -z "$isMounted" ] 
 then
-    echo "Service_MOUNT: Error de montado, el dispositivo $Name_Dev no existe o no se puede montar"
+    echo "ERROR: El dispositivo $Name_Dev no existe o no se puede montar"
     exit 1
 fi
 
-# Check mount point 
+# Comprobar Punto de Montaje
 
 if [ -z "$Mount_Point" ] 
 then
-	echo "Service_MOUNT: Error de montado, falta de punto de montaje"
+	echo "ERROR: Falta de punto de montaje"
 	exit 1
 fi
 
-# Creat a directory with intermediate (OPTIONS: -p, --parents)
+# Crear un direcctorio con intermediarios (Opciones: -p, --parents)
 
 mkdir -p $Mount_Point > /dev/null
 
-# Check configuration on fstab and mount
+# Comprobar existencia del montaje en /etc/fstab
 
 grep -o "$Name_Dev" /etc/fstab > /dev/null 
 
 if [ $? -ne 0 ]
 then 
-    echo "Service_MOUNT: El dispositivo no esta en fstab" 
+    echo "Servicio MOUNT: El dispositivo no está en fstab" 
     echo "$Name_Dev $Mount_Point auto auto 0 0" >> /etc/fstab 
 else 
-    echo "Service_MOUNT: El dispositivo $Name_Dev ya esta en /etc/fstab"
+    echo "Servicio MOUNT: El dispositivo $Name_Dev ya está en /etc/fstab"
 fi
  
 if [ $? -eq 0 ]
 then 
-    echo "Service_MOUNT: El dispositivo $Name_Dev adjunto en /etc/fstab"
+    echo "Servicio MOUNT: El dispositivo $Name_Dev adjuntado a /etc/fstab"
 fi
 
-# mount all file systems mentioned in fstab
+# Montar todos los ficheros de sistema mencionados en fstab
 
 mount -a > /dev/null
 
 if [ $? -eq 0 ]
 then
-    echo "Service_MOUNT: Servicio Montaje completado"
-    echo "Service_MOUNT: Dispositivo $Name_Dev montado correctamente en $Mount_Point"
+	echo "Servicio MOUNT: Dispositivo $Name_Dev montado correctamente en $Mount_Point"
+    echo "Servicio MOUNT: Servicio Montaje completado"    
 else 
-    echo "Service_MOUNT: Error al finalizar montaje"
+    echo "ERROR: Error al finalizar montaje"
+    exit 1
 fi 
